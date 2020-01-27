b997bcc65aeaacf18b7e9131df59a58946f91323
Pedro Crespo <pedro.crespo.martin@gmail.com>
2020-01-27 17:28:06 -0600

    2701

--- SGXC/SGXC/Services/StatsService.cs
+++ SGXC/SGXC/Services/StatsService.cs
@@ -3,6 +3,7 @@ using SGXC.Models.Helper;
 using SQLite;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,19 +15,25 @@ namespace SGXC.Services
 
         public SQLiteAsyncConnection Connection { get; set; }
         public IEventServices EventServices { get; set; }
+        public ITimeServices TimeServices { get; set; }
 
-        public StatsService(ISQLite dbservices, IEventServices eventServices)
+        public StatsService(ISQLite dbservices, IEventServices eventServices, ITimeServices timeServices)
         {
             this.dbservices = dbservices;
             Connection = dbservices.GetConnection();
             EventServices = eventServices;
+            TimeServices = timeServices;
         }
 
         public async Task<List<PracticeData>> GetPracticeData(int runnerid)
         {
-            //Select events that are practices;
-            var listofpractices = new List<Event>();            
-            var events = await Connection.Table<RunnerEvent>().Where(r => r.RunnerId == runnerid).ToListAsync();            
+            var distances = new List<string> { "100m", "200m", "300m", "400m (1/4 mile)", "600m" ,"800m (1/2 mile)",
+                                                        "1K","1.2K","1.5K","1 mile","2K", "2 mile", "3 mile", "5K", "10K"};
+            var PracticeData = new List<PracticeData>();
+
+            //Select run practices of a runner;
+            var listofpractices = new List<Event>();
+            var events = await Connection.Table<RunnerEvent>().Where(r => r.RunnerId == runnerid).ToListAsync();
             foreach (var @event in events)
             {
                 var practiceevent = await EventServices.GetEventById(@event.EventId);
@@ -36,10 +43,33 @@ namespace SGXC.Services
                 }
             }
 
+            // Create Practice Data
+            for (int i = 0; i < distances.Count; i++)
+            {
+                var data = new PracticeData { Distance = distances[i], PracticeResults = await Getlistof(listofpractices, distances[i]) };
+                PracticeData.Add(data);
+            }
+            return PracticeData;
 
-
-            throw new NotImplementedException();
         }
+
+        //Creates a list of practice result for each distance of every event
+        private async Task<List<PracticeResult>> Getlistof(List<Event> listofpractices, string distance)
+        {
+            var PracticeResults = new List<PracticeResult>();
+            foreach (var practice in listofpractices)
+            {
+                var listoftimes = await TimeServices.GetListOfTimesbyEventId(practice.Id);
+                listoftimes.Select(d => d.Distance == distance);
+                foreach (var time in listoftimes)
+                {
+                    var newpracticeresult = new PracticeResult { Date = practice.RaceDate, Time = time.Times };
+                    PracticeResults.Add(newpracticeresult);
+                }
+            }
+            return PracticeResults;
+        }
+
 
         public async Task<List<RaceData>> GetRaceData(int runnerid)
         {
--- SGXC/SGXC/ViewModels/RunnerStatsViewModel.cs
+++ SGXC/SGXC/ViewModels/RunnerStatsViewModel.cs
@@ -2,6 +2,8 @@
 using Prism.Mvvm;
 using Prism.Navigation;
 using SGXC.Models;
+using SGXC.Models.Helper;
+using SGXC.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,10 @@ namespace SGXC.ViewModels
 {
     public class RunnerStatsViewModel : BindableBase, INavigationAware
     {
+        INavigationService NavigationService { get; set; }
+
+        IStatsService StatsService { get; set; }
+
         private Runner runner = new Runner();
         public Runner Runner
         {
@@ -17,9 +23,22 @@ namespace SGXC.ViewModels
             set { SetProperty(ref runner, value); }
         }
 
-        public RunnerStatsViewModel()
+        private List<PracticeData> practicedata;
+        public List<PracticeData> PracticeData
         {
+            get { return practicedata; }
+            set { SetProperty(ref practicedata, value); }
+        }
 
+        public RunnerStatsViewModel(INavigationService navigationService, IStatsService statsService)
+        {
+            NavigationService = navigationService;
+            StatsService = statsService;
+
+        }
+        public async void LoadPracticeData(int id)
+        {
+            PracticeData = await StatsService.GetPracticeData(id);
         }
 
         public void OnNavigatedFrom(INavigationParameters parameters)
@@ -33,7 +52,7 @@ namespace SGXC.ViewModels
 
             {
                 Runner = (Runner)parameters["runner"];
-
+                LoadPracticeData(Runner.Id);                
             }
         }
     }
